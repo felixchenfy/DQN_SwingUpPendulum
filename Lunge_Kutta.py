8759b2aaeb323850a7eae465aa4bb601a4988497
Feiyu Chen <felixchenfy@gmail.com>
2018-11-22 20:34:40 -0600

    Add force input

---
+++ Lunge_Kutta.py
@@ -0,0 +1,116 @@
+# copied from here:
+# https://www.codeproject.com/Tips/792927/Fourth-Order-Runge-Kutta-Method-in-Python
+
+import math
+
+def rKN(x, fx, n, hs):
+   k1 = []
+   k2 = []
+   k3 = []
+   k4 = []
+   xk = []
+   for i in range(n):
+       k1.append(fx[i](x)*hs)
+   for i in range(n):
+       xk.append(x[i] + k1[i]*0.5)
+   for i in range(n):
+       k2.append(fx[i](xk)*hs)
+   for i in range(n):
+       xk[i] = x[i] + k2[i]*0.5
+   for i in range(n):
+       k3.append(fx[i](xk)*hs)
+   for i in range(n):
+       xk[i] = x[i] + k3[i]
+   for i in range(n):
+       k4.append(fx[i](xk)*hs)
+   for i in range(n):
+       x[i] = x[i] + (k1[i] + 2*(k2[i] + k3[i]) + k4[i])/6
+   return x
+
+
+def rK3(a, b, c, fa, fb, fc, hs):
+    a1 = fa(a, b, c)*hs
+    b1 = fb(a, b, c)*hs
+    c1 = fc(a, b, c)*hs
+    ak = a + a1*0.5
+    bk = b + b1*0.5
+    ck = c + c1*0.5
+    a2 = fa(ak, bk, ck)*hs
+    b2 = fb(ak, bk, ck)*hs
+    c2 = fc(ak, bk, ck)*hs
+    ak = a + a2*0.5
+    bk = b + b2*0.5
+    ck = c + c2*0.5
+    a3 = fa(ak, bk, ck)*hs
+    b3 = fb(ak, bk, ck)*hs
+    c3 = fc(ak, bk, ck)*hs
+    ak = a + a3
+    bk = b + b3
+    ck = c + c3
+    a4 = fa(ak, bk, ck)*hs
+    b4 = fb(ak, bk, ck)*hs
+    c4 = fc(ak, bk, ck)*hs
+    a = a + (a1 + 2*(a2 + a3) + a4)/6
+    b = b + (b1 + 2*(b2 + b3) + b4)/6
+    c = c + (c1 + 2*(c2 + c3) + c4)/6
+    return a, b, c
+
+
+def test_3_dimension():
+
+    # y¨=μ(1−y^2)y˙−y+Asin(ωt)
+    # y¨=0.9(1−y^2)y˙−y+1*sin(0.5*t)
+
+    # Let:x  =( dy,  y, wt)
+    #  dx/dt =(ddy, dy,  w)
+
+    # a: dy
+    # b: y
+    # c: t
+    # fa: d(dy)/dt = equation
+    # fb: d(y)/dt = a
+    # fa: d(wt)/dt = w
+
+    def fa2(a, b, c):
+        return 0.9*(1 - b*b)*a - b + math.sin(c)
+
+    def fb2(a, b, c):
+        return a
+
+    def fc2(a, b, c):
+        return 0.5 # d(wt)/dt=w
+
+    def VDP2():
+        a, b, c, hs = 1, 1, 0, 0.05
+        for i in range(20000):
+            a, b, c = rK3(a, b, c, fa2, fb2, fc2, hs)
+        return a,b,c
+
+    a,b,c=VDP2()
+    print(a,b,c)
+
+def test_N_dimension():
+    def fa1(x):
+        return 0.9*(1 - x[1]*x[1])*x[0] - x[1] + math.sin(x[2])
+
+    def fb1(x):
+        return x[0]
+
+    def fc1(x):
+        return 0.5
+
+    def VDP1():
+        f = [fa1, fb1, fc1]
+        x = [1, 1, 0]
+        hs = 0.05
+        for i in range(20000):
+            x = rKN(x, f, 3, hs)
+        return x
+
+    x=VDP1()
+    print(x)
+
+if __name__=="__main__":
+
+    test_3_dimension()
+    # test_N_dimension()
--- pendulum_simulation.py
+++ pendulum_simulation.py
@@ -1,12 +1,11 @@
 
-
-
 import tkinter as tk
 import numpy as np
 import math
 import os
 from pendulum_window import myWindow
 import time
+from Lunge_Kutta import rK3  # Lunge-Kutta Integration method
 
 '''
 # Simulation
@@ -29,93 +28,182 @@ dq[k+1]=dq[k]+ddq*dt
 q[k+1]=q[k]+dq*dt
 '''
 
-class Pendulum(object):
-    def __init__(self, dt):
-        self.m=1.0 # mass
-        self.R=1.0 # length
-        self.i=1.0/3*self.m*self.R**2 # inertia 
 
-        self.g=9.8
-        self.cf=0.01 # coef of friction
-        # self.friction = lambda cf, dq: -cf*dq
-        self.fNoise=0.001
-        self.q=0.0
-        self.dq=0.0
-        self.ddq=0.0
+class Pendulum(myWindow):
+    def __init__(self, dt_sim):
+        super(Pendulum, self).__init__()
+
+        self.m = 1.0  # mass
+        self.R = 1.0  # length
+        self.i = 1.0/3*self.m*self.R**2  # inertia
+
+        self.g = 9.8
+        self.cf = 0.1  # coef of friction, where friction = - cf * dq
+        self.fNoise = 0.001
+
+        self.q = 0.0
+        self.dq = 0.0
+        self.ddq = 0.0
 
         # simulation params
-        self.dt=0.01
-        self.t0=time.time()
-        self.t_=0.0
+        self.dt_sim = dt_sim
+        self.t0 = time.time()
+        self.t_ = 0.0
+
+        # user input
+        self.torque = 0
+        # self.torque_effect_time=0.1
 
         # pre-compute some qualities
-        self.I=4*self.i + self.m*self.R**2
-        self.gmR=self.g*self.m*self.R
+        self.I = 4*self.i + self.m*self.R**2
+        self.gmR = self.g*self.m*self.R
 
-        return
+    def update_q_from_window(self):
+        self.q = -self.link1_theta
 
-    def compute_ddq(self):
-        fNoise=self.fNoise*(np.random.random()*2-2)
-        self.ddq = 4*(fNoise - self.gmR/2*math.cos(self.q) - self.cf*self.dq)/self.I
-        return self.ddq
+    def assign_q_to_window(self):
+        self.link1_theta = -self.q
 
-    def update_q_dq(self):
-        self.dq=self.dq+self.ddq*self.dt
-        self.q=self.q+self.dq*self.dt
-        return self.q, self.dq
+    def compute_ddq(self, q, dq):
+        fNoise = self.fNoise*(np.random.random()*2-2)
+        F = fNoise+self.torque-self.cf*dq # total external torque applied on joint 1
+        ddq = 4*(F - self.gmR/2*math.cos(q))/self.I
+        return ddq
+
+    def update_states(self): # integrate ddq to get q and dq
+        self.update_q_and_dq_()
+
+    def update_q_and_dq_(self):
+        q = self.q
+        dq = self.dq
+        ddq = self.compute_ddq(q, dq)
+        dt = self.dt_sim
+
+        if 0:  # 1st order: Euler-Integration
+            q += dq*dt
+            dq += ddq*dt
+
+        else:  # 4th order: Runge-Kutta method
+            if not hasattr(self, 'fa_'):
+                # vec=(a,b,c)=(dq,q,t)
+                # f(a,b,c)=(da,db,dc)=(ddq,dq,1)
+                # fa: d(dq)/dt
+                # fb: d(q)/dt
+                # fa: d(t)/dt
+                self.fa_ = lambda a, b, c: self.compute_ddq(q=b, dq=a)
+                self.fb_ = lambda a, b, c: a
+                self.fc_ = lambda a, b, c: 1
+            a = dq
+            b = q
+            c = 0  # here ddq() is not depend on t, so let t=0
+            dq, q, _ = rK3(a, b, c, self.fa_, self.fb_, self.fc_, dt)
+
+        self.q = q
+        self.dq = dq
 
     def reset_time(self):
-        self.t0=time.time()
+        self.t0 = time.time()
 
     def get_time(self):
         return time.time()-self.t0
 
-INTERVAL_PRINT=1.0 # print for every ?? seconds
+    def set_torque(self, torque):
+        self.torque=torque
+
+    def event_KeyPress(self, e):  # overload
+        c = e.char.lower()
+
+        # rotate link
+        dtheta = 0.1
+        if c == "a":
+            self.rotate_link1(-dtheta)
+        elif c == "d":
+            self.rotate_link1(dtheta)
+
+        # apply torque
+        torque_maginitude=1
+        if c == "j":
+            self.set_torque(-torque_maginitude)
+        elif c == "k":
+            self.set_torque(torque_maginitude)
+
+        # restart
+        elif c == 'q':
+            self.restart()
+        return
+
+    def event_KeyRelease(self, e):
+        c = e.char.lower()
+        # reset torque
+        if c == "j":
+            self.set_torque(0)
+        elif c == "k":
+            self.set_torque(0)
+        return
+
+    def display_text(self): # overload
+        str_text = "User input:\n"
+        # str_text += "A,D: rotate link1\n"
+        # str_text += "W,S: rotate link2\n"
+        str_text += "J,K: apply torque to link1\n"
+        str_text += "Q: reset game\n"
+        # str_text+="E/R: enable/disable simulation\n" # the sim engine is in "pendulum_env"
+        self.display_text_(str_text)
+
+PRINT_INTERVAL = 1.0  # print for every PRINT_INTERVAL seconds
+
 
 def update():
     pendulum.reset_time()
 
-    MAX_ITE=int(1000.0/dt_sim)
-    ite=0
-    while ite<MAX_ITE:
-        ite+=1
-        
-        if ite%(dt_disp/dt_sim)==0:
-            mw.canvas.update()
+    MAX_ITE = int(1000.0/dt_sim)
+    ite = 0
+    while ite < MAX_ITE:
+        ite += 1
 
-        if ite%(INTERVAL_PRINT/dt_sim)==0:
+        # update window
+        if ite % (dt_disp/dt_sim) == 0:
+            pendulum.canvas.update()
+
+        # print time and q
+        if ite % (PRINT_INTERVAL/dt_sim) == 0:
             print("t=%.2f, ddq=%.2f" %
-                (pendulum.get_time(), pendulum.ddq))
+                  (pendulum.get_time(), pendulum.ddq))
 
-        q_pre,dq_pre=pendulum.q,pendulum.dq
-        pendulum.compute_ddq()
-        pendulum.update_q_dq()
-        q_cur,dq_curr=pendulum.q,pendulum.dq
+        # read current q from window
+        # (because user might reset the link's position)
+        pendulum.update_q_from_window()
 
-        delta_theta=q_cur-q_pre
-        output_for_display(delta_theta)
+        # do simulation
+        pendulum.update_states()
+
+        # output to the window
+        pendulum.assign_q_to_window()
+        pendulum.rotate_link1_to(pendulum.link1_theta)
 
         # sleep
-        t_sleep=ite*dt_sim-pendulum.get_time()
-        if t_sleep>0:
+        t_sleep = ite*dt_sim-pendulum.get_time()
+        if t_sleep > 0:
             time.sleep(t_sleep)
 
-def output_for_display(delta_theta):
-    # the window's coordinate: x to right, y to down.
-    # which is different from world.
-    # So, I need to change coordinate
-    delta_theta = - delta_theta
 
-    mw.rotate_link1(delta_theta)
-
-if __name__=="__main__":
+if __name__ == "__main__":
     # b = tk.Button(window, text='move', command=moveit).pack()
-    os.system('xset r on')
+    try:
+        os.system('xset r off')
+    except:
+        None
 
-    dt_sim=0.01
-    dt_disp=0.05
-    pendulum=Pendulum(dt_sim)
-    mw=myWindow()
+    dt_sim = 0.005
+    dt_disp = 0.05
+
+    pendulum = Pendulum(dt_sim)
+    pendulum.window.after(10, update)
+    pendulum.mainloop()
     
-    mw.window.after(10, update)
-    tk.mainloop()
+    try:
+        os.system('xset r on')
+    except:
+        None
+
+
