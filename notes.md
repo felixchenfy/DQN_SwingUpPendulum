a2397e5f133f0a751fa9d5f9259d28a90f4b1937
Feiyu Chen <felixchenfy@gmail.com>
2018-11-27 16:05:36 -0600

    Oh my god, it workspython3 run_this_to_train.py ! I should save it before further training.

--- notes.md
+++ notes.md
@@ -7,7 +7,7 @@ https://github.com/keras-rl/keras-rl
 https://github.com/mws262/MATLAB-Reinforcement-Learning-Pendulum/blob/master/QlearnPend.m
 
 
-# states=x,y,dtheta, for continuousty 
+# states=x,y,dq, for continuousty 
 11.25：我用10×10的relu试了下； inverted_pend,500k收敛；down_pend,一晚上，跑不出来
 11.26：我把力改成了2-->3，网络10*15*10
     inverted_pend, 80k就收敛的很好了
@@ -20,8 +20,21 @@ https://github.com/mws262/MATLAB-Reinforcement-Learning-Pendulum/blob/master/Qle
     问题是：DQN里与没有个机制，在到了终点以后，把最后那部分，反复训练？
     
 11.26周一晚上跑了一晚上，还是没收敛。
-我把最大速度从5改成了4，把上层区间（上下层reward不一样）设为了 +-2/3*pi，再跑一下。还是不行。
-我发现，如果用x,y,dq,就会把inv_pend给搞坏掉。所以还是用q,dq当成状态吧
+我把最大速度从5改成了4，把上层区间（上下层reward不一样）设为了 +-2/3*pi，再跑一下。
+    结果：差不多能swing个100度了，但是没法停在上面。
+这时候我试了一下inv_pend，发现跑了150000都没收敛。估计是x,y,的权重被ruin了吧。
+所以还是把q,dq当成状态，重新训练网络吧。
+
+# states=q, dq
+继续之前的网络：10*15*15*10
+inv_pend:固定初始位置，200k次收敛
+            随机初始位置，再200k
+            
+# notes on reward：theta^2
+r = lambda theta_, dq_: -(theta_**2 + 0.01*dq_**2)
+如果theta**3，那么在inverted_pendulum中，当theta很小时，reward差异就很小，导致dq的比重增加。
+dq比重增加后，pend就倾向于在delta_theta的位置保持不动，而不会使theta=0。
+结论：还是用theta**2吧。
 
 
 
--- run_this_to_train.py
+++ run_this_to_train.py
@@ -5,18 +5,22 @@ from math import pi
 import time, math
 import numpy as np
 
-
+Q_OFFSET=pi/2
 def trans_angle(theta):# transform angle to [-pi, pi). When link1 is vertically upward, angle=0
-    return (theta-pi/2+pi) % (2*pi)-pi
-    
+    return (theta-Q_OFFSET+pi) % (2*pi)-pi
+# theta=pi/2, return 0
+# theta=pi/2+0.1, return 0.1
 
 # Choose a scenario
-Inverted_Pendulum=True
+Inverted_Pendulum=False
 Swing_Up_Pendulum=not Inverted_Pendulum
 
 # Whether use random initial position (q, dq)
-Random_Init=False
-Max_Steps_Per_Episode=4000
+Random_Init=True
+if Inverted_Pendulum:
+    Max_Steps_Per_Episode=1000
+else:
+    Max_Steps_Per_Episode=4000
 
 # set mode
 Training_Mode=True
@@ -44,7 +48,10 @@ if Testing_Mode:
 
 class RL_Pendulum(Pendulum):
     def __init__(self, q_init, dq_init):
-        super(RL_Pendulum, self).__init__(q_init, dq_init)
+        self.state0_init=q_init
+        self.state1_init=dq_init
+        super(RL_Pendulum, self).__init__(q_init+Q_OFFSET, dq_init)
+
         self.action_space = ['o', 'l', 'r']
         # actions: (1) no torque; (2) torque to left(-); (3) toruqe to right(+);
         self.n_actions = len(self.action_space)
@@ -53,16 +60,22 @@ class RL_Pendulum(Pendulum):
         return
 
     def reset(self, q1_init=None, dq1_init=None):
-        super(RL_Pendulum, self).reset(q1_init, dq1_init)
+        super(RL_Pendulum, self).reset(self.state0_init+Q_OFFSET, self.state1_init)
         return self.get_states_for_DL()
 
-    def get_states_for_DL(self):
-        q=self.q
+    def get_states(self):
+        q=trans_angle(self.q)
         dq=self.dq
-        x=math.cos(q)
-        y=math.sin(q)
-        return np.array([x, y, dq])
-        # return np.array([q, dq])
+        return np.array([q, dq])
+
+    def get_states_for_DL(self):
+        if 0: # this is a test of using x,y to replace q. But failed. Not so good.
+            x=math.cos(self.q)
+            y=math.sin(self.q)
+            return np.array([x, y, self.dq])
+        else:
+            q=trans_angle(self.q)
+            return np.array([q, self.dq])
 
     def step(self, action, time):
         self.action=action
@@ -86,8 +99,8 @@ class RL_Pendulum(Pendulum):
             # for this problem, there is no completion of episode
             done=False 
         elif Inverted_Pendulum:
-            theta = trans_angle(states_new[0])
-            if abs(theta)>pi/6:
+            q = states_new[0]
+            if abs(q)>pi/2:
                 done=True
             else:
                 done=False
@@ -97,15 +110,17 @@ class RL_Pendulum(Pendulum):
 
     def compute_reward(self, states):
         q=states[0]
-        dq=abs(states[1])
-        theta = trans_angle(q)
+        dq=states[1]
+        # print(q, dq)
+        q=abs(q)
+        dq=abs(dq)
 
         # reward
-        r = lambda theta_, dq_: -(abs(theta_)**3 + 0.01*dq_**2)
-        if abs(theta)<pi*2/3:
-            reward= r(theta,dq)
+        r = lambda q_, dq_: -(q_**2 + 0.01*dq_**2)
+        if q<pi*2/3:
+            reward= r(q,dq)
         else:
-            reward= r(pi*2/3,4)-(4.0-dq)**2
+            reward= r(pi*2/3,4)-0.1*(4.0-dq)**2
 
         return reward
 
@@ -135,15 +150,20 @@ def run_pendulum(observation_interval_=0.01):
 
     while True:
         episode+=1
-
         # initial states
         if Random_Init is True:
+            dq_random_max=2
             if Inverted_Pendulum:
-                q1_init=(np.random.random()-0.5) *pi/3 /1.1+pi/2
-                states = env.reset(q1_init=q1_init,dq1_init=0)
+                q_random_max=pi/3
+                q1_init=(np.random.random()-0.5)*2 *q_random_max
+                dq1_init=np.random.random()*dq_random_max
+                if q1_init>0: # make init velocity towards the center
+                    dq1_init=-dq1_init
+                states = env.reset(q1_init=q1_init,dq1_init=dq1_init)
             if Swing_Up_Pendulum:
-                q1_init=np.random.random()*pi
-                dq1_init=(np.random.random()-0.5)*4
+                q_random_max=pi
+                q1_init=(np.random.random()-0.5) *2*q_random_max
+                dq1_init=(np.random.random()-0.5)*2*dq_random_max
                 states = env.reset(q1_init=q1_init,dq1_init=dq1_init)
         else:
             states = env.reset()
@@ -167,7 +187,7 @@ def run_pendulum(observation_interval_=0.01):
 
             RL.store_transition(states, action, reward, observation_)
 
-            if (step > 200) and (step % 20 == 0):
+            if (step > 200) and (step % 10 == 0):
                 RL.learn()
 
             # swap states
@@ -212,9 +232,9 @@ def run_pendulum(observation_interval_=0.01):
 if __name__ == "__main__":
     # pendulum game
     if Swing_Up_Pendulum:
-        q_init=-pi/2
+        q_init=pi
     elif Inverted_Pendulum:
-        q_init=pi/2
+        q_init=0
     env = RL_Pendulum(q_init=q_init, dq_init=0)
 
     # deep Q-network
